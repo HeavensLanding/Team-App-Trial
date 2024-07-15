40f630b02c9b370e9176f6eb7d7ecba444116df4
HeavensLanding <rasmussencourtney0103@gmail.com>
2024-07-15 15:15:56 -0600

    x

---
+++ index.js
@@ -0,0 +1,22 @@
+class Member {
+    constructor(name, position) {
+        this.name = name;
+        this.position = position;
+    }
+}
+
+class Team {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.members = {};
+    }
+
+    addMember(member) {
+        this.members.push(member);
+    }
+
+    deleteMember(member) {
+        let index = this.members.indexOf(member);
+    }
+}
